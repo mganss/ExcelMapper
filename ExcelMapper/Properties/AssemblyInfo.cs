83dbbe2415a7f79f311c2dd9e626f2e83c6f2164
Michael Ganss <michael@ganss.org>
2015-11-29 13:32:34 +0100

    Add appveyor.yml

--- ExcelMapper/Properties/AssemblyInfo.cs
+++ ExcelMapper/Properties/AssemblyInfo.cs
@@ -22,14 +22,7 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0b860688-c17c-416f-98fe-8292c891e59b")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
+// assembly version information will be reset by AppVeyor
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
