79d83452be8bfda67bf025fdc74c912edb1f0ba8
NdubuisiJr <ndubuisijrchukuigwe@gmail.com>
2020-02-26 12:40:49 +0100

    1. Added IsPrimitiveTypeExceptionTest class 2. Removed unused 'using' statements 3. Added a test case to comfirm the exception message format for IsPrimitiveTypeException

--- ExcelMapper/ExcelMapper.cs
+++ ExcelMapper/ExcelMapper.cs
@@ -4,12 +4,10 @@ using NPOI.XSSF.UserModel;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Data;
 using System.IO;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
-using System.Text;
 using Ganss.Excel.Exceptions;
 
 namespace Ganss.Excel
